bab9695e3fac30b83a891206f56746ea4c39a2b0
iillumi <wnstjr218@naver.com>
2021-05-18 23:11:53 +0900

    use DFS

--- 1012.c
+++ 1012.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+void DFS(int row, int colume, int arry[][colume]);
+
 int main () {
 	int testcase;
 	int rowsize, columsize; // input arry size
 	int cabbage; // number of cabbage
 	int row, colum; // set cabbage point
+	int count; // bug's num
 	scanf ("%d", &testcase);
-
+	scanf ("%d %d %d", &columsize, &rowsize, &cabbabge);
+	int arry[rowsize][columszie]={0,};
+	for (int i=0;i<testcase;i++){ // set cabbage
+		scanf("%d %d", &colum, &row);
+		arry[row][colum]=1;
+	}
 	return 0;
 }
 
