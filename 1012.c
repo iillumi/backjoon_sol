6c8526a2efbbe47511d7107dc52f05bee6d227bf
iillumi <wnstjr218@naver.com>
2021-05-18 23:50:30 +0900

    use DFS

--- 1012.c
+++ 1012.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int rowsize, columsize; // arry size
-int arry[50][50];
+int arry[50][50]={0,};
 
 void DFS(int row, int colume);
 
@@ -16,9 +16,15 @@ int main () {
 		scanf("%d %d", &colum, &row);
 		arry[row][colum]=1;
 	}
+	for (int i=0;i<row;i++) {
+		for (int j=0;j<colum;j++){
+			if(arry[row][colum]==1){
+				DFS(row, colum);
+		}
+	}
 	return 0;
 }
 
 void DFS (int row, int colume){
-
+	
 }
