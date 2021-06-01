01e42c3e93ca9891c19a893dfcde5959aeb8e7e1
iillumi <wnstjr218@naver.com>
2021-06-01 22:48:34 +0900

    use C

--- 1016.c
+++ 1016.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
 int main () {
 	long long int min, max;
 	int count=0;
-	int arry[1000000]={1,};
-	double cal;
+	int arry[1000000]={0,};
+	long long int num;
 	scanf("%lld %lld", &min, &max);
 	for (long long int i=2;i*i<=max;i++) {
+		num=min/(i*i);
+		if(num*i*i!=min) num++;
+		for(long long int j=num; i*i*j<=max;j++) {
+			arry[j*i*i-min]=1;
+		}
 	}
-
-	for (int i=0;i<(max-min);i++) {
-		if (arry[i]) count++;
+	for (int i=0;i<(max-min+1);i++) {
+		if(!arry[i]) count++;
 	}
 	printf ("%d\n", count);
 
