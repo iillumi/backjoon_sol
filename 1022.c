63be35de620f790b34e9b475bad904e5514ac996
iillumi <wnstjr218@naver.com>
2021-07-03 22:57:19 +0900

    use C

--- 1022.c
+++ 1022.c
@@ -5,36 +5,50 @@
 #include <math.h>
 
 int main () {
-	int r1,c1,r2,c2;
-	int arry [50][5]={0,};
-	int num=1;
-	int count=0;
-	int x=0;
-	int y=0;
+        int r1,c1,r2,c2;
+        int arry [50][5]={0,};
+        int num=1;
+        int count=0;
+        int row=0;
+        int colum=0;
 
-	scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
-	if(r1<=y && r2>=y && c1<=x && c2>=x) 
-		arry[y-r1][x-c1]=num;
+        scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
+        if(r1<=0 && c1<=0 && r2>=0 && c2>=0) arry[row-r1][colum-c1]=num;
 
+        for (int i=r1;i<=r2;i++){
+                for (int j=c1;j<=c2;j++){
+                        row=i-r1;
+                        colum=j-c1;
 
-	
-
-	while (num>0){
-		num/=10;
-		count++;
-	}
-	for(int i=0;i<=r2-r1;i++){
-		for (int j=0;j<=c2-c1;j++){
-			printf ("%*d ", count, arry[i][j]);
-		}
-		printf("\n");			
-	}
+                        if (i<0 && abs(i)>=abs(j)) arry[row][colum]=4*i*i+1+i-j;
+                        else if (j<0 && abs(i)<abs(j)) arry[row][colum]=4*j*j+1+i-j;
+                        else if (i>0 && abs(i)>=abs(j)) arry[row][colum]=4*i*i+3*i+1+j;
+                        else if (j>0 && abs(i)<abs(j)) arry[row][colum]=4*j*j-3*j+1-i;
 
 
-	return 0;
+                        if(arry[row][colum]>num) num=arry[row][colum];
+                }
+        }
+
+
+
+        while (num>0){
+                num/=10;
+                count++;
+        }
+        for (int i=0;i<=r2-r1;i++){
+                for (int j=0;j<=c2-c1;j++){
+                        printf ("%*d ", count, arry[i][j]);
+                }
+                printf("\n");
+        }
+
+
+        return 0;
 }
 
-// 피라미드 형식으로 보이며
-// 4*r*r+1+r-c / r<0 |r|>=|c|
-// 4*c*c+1+r-c / c<0 |c|>=|r|
-//
+// 피라미드 형식으로 보이며'r is row / c is colum'
+// 4*r*r+1+r-c   / r<0 && |r|>=|c|        up
+// 4*c*c+1+r-c   / c<0 && |r|<|c|         left
+// 4*r*r+3*r+1+c / r>0 && |r|>=|c|        down
+// 4*c*c-3*c+1-r / c>0 && |r|<|c|         right
