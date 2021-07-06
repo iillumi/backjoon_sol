2cc82c179d613d03eb829c4db2a574992832b091
iillumi <wnstjr218@naver.com>
2021-07-06 23:13:27 +0900

    not finished

--- 2042.c
+++ 2042.c
@@ -1,16 +1,23 @@
 // refer to https://blog.naver.com/ndb796/221282210534
 
 #include <stdio.h>
-
+	
 int main () {
 	int n,m,k,a,b,c;
 	scanf("%d %d %d", &n, &m, &k);
-	long long int arry[n+1]={0,};
-	long long int sum=0;
-	for(int i=1;i<=n;i++) {
+	long long int arry[n+1];
+	for (int i=1;i<=n;i++){
 		scanf("%lld", &arry[i]);
 	}
-	// segment tree 작성
+	
+	for (int i=0;i<m+k;i++) {
+		scanf("%d %d %d", &a, &b, &c);
+		if(a==1) {
+		}
+		else {
+		}
+	}
+
 
 	return 0;
 }
