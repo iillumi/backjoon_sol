4e80d03046dbf1bee4eee9bde59f2ba46b9ec213
iillumi <wnstjr218@naver.com>
2021-07-10 23:53:19 +0900

    use C

--- 1112.c
+++ 1112.c
@@ -1,43 +1,33 @@
-//양수 진법 변환 O
-//양수를 -10진법 변환 O
-//나머지 X
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 int main (){
-        int x,b;
-        int remainder=0;
-        int count=0;
-        scanf("%d %d", &x, &b);
-        while (abs(x)>=abs(b)){
-                if(b>0) remainder+=(x%b)*pow(10,count);
-                else {
-					if(count%2==0){
-						remainder+=(x%b)*pow(10,count);
-					}
-					else{
-						if(x%b!=0) {
-							remainder+=pow(10,count+1)+pow(10,count+1)-(x%b*pow(10,count));
-						}
-					}
-				}
-                x/=abs(b);
-                count++;
-        }
+    int x,b;
+	int i=0;
+	char remainder[40]={0,};
+    scanf("%d %d", &x, &b);
+	if(x<0 && b>0) {
+		remainder[0]='-';
+		x=-x;
+	}
+	if(x==0) printf("0");
+    while (x){
+		if(b>0) {
+			remainder[++i]=x%b;
+		}
+		else {
+			remainder[++i]+=x%b;
+			if(remainder[i]<0){
+				remainder[i]-=b;
+			}
+			x-=remainder[i];
+		}
+		x/=b;
+	}
 
-        if(b>0) remainder+=(x%b)*pow(10,count);
-        else {
-                if(count%2==0){
-						remainder+=(x%b)*pow(10,count);
-					}
-					else{
-						remainder+=pow(10,count+1)+pow(10,count+1)-(x%b*pow(10,count));
-					}
-        }
-	
-        printf ("%d\n", remainder);
-        return 0;
+	if(remainder[0]=='-') printf("-");
+	for(int j=i;j>0;j--){
+		printf ("%d", remainder[j]);
+	}
+	printf("\n");
+	return 0;
 }
-
