5149a14ebb82c1a5eb7e400308d975439b1759d3
iillumi <wnstjr218@naver.com>
2021-07-14 23:21:11 +0900

    use C

---
+++ 4355.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+        long long int n;
+        long long int count=0;
+        while(1){
+		scanf("%lld",&n);
+		if(n==0) break;
+		count=n;
+		for (long long int i=2;i*i<=n;i++) {
+                	if(n%i==0){
+                        	while (n%i==0) n/=i;
+	                        count/=i;
+        	                count*=(i-1);
+        	        }
+        	}
+        	if(n!=1){
+		       	count/=n;
+        		count*=(n-1);
+	        }
+        	printf ("%lld\n", count);
+	}
+        return 0;
+}
