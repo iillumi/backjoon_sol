c8135820a42dd3c6132eba1c7577473aa7fde8f2
iillumi <wnstjr218@naver.com>
2021-07-23 23:30:42 +0900

    not finished

--- 16233.c
+++ 16233.c
@@ -1,25 +1,29 @@
 // n/9했을때 안되는 수들 찾는 식 만들기
+// 시간초과....
 #include <stdio.h>
 
 int main () {
 	int t,n;
 	int a,b;
-	int count=10;
+	int count;
 	scanf("%d", &t);
 	for (int i=0;i<t;i++) {
 		scanf("%d", &n);
+		count=0;
 		if(n%9==0){
-			for(int i=n;i<n+100;i++){
-				a=i;
+			for(int j=n;j<n+100;j++){
+				a=j;
 				b=0;
+				count++;
 				while(a){
 					b+=a%10;
 					a/=10;
 				}
-				if(n+b==i){
-					printf("%d ",i);
+				if(n+b==j){
+					printf("%d ",j);
 					break;
 				}
+				if(count==100) printf("-1 ");
 			}
 		}
 		else printf("-1 ");
