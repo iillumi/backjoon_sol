9eb6380860b069b85a00cfcb38258cff4ffd5b7f
iillumi <wnstjr218@naver.com>
2022-03-11 16:17:25 +0900

    properly formed parentheses

--- study_assignment/9012.cpp
+++ study_assignment/9012.cpp
@@ -14,13 +14,13 @@ int main() {
         index = 0;
         a = 0;
         while (1) {
-            if (input[index] == '(') a++;
-            else if (input[index] == ')') a--;
-            if (a < 0) {
+            if (input[index] == '(') a++; // '(' 일때마다 값을 +
+            else if (input[index] == ')') a--; // ')' 일때마다 값을 -
+            if (a < 0) { // ')'가 '('보다 많이 나오면 괄호가 형성되지 않음
                 cout << "NO" << endl;
                 break;
             }
-            if (input[index] == NULL) break;
+            if (input[index] == NULL) break; // 마지막 행 '\0'을 만나면 break
             index++;
         }
         if (a > 0) cout << "NO" << endl;
